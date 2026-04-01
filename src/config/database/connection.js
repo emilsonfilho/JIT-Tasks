06429673487b3f30a6db4e00ed345cbf4774c50d
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-04-01 14:36:08 -0300

    chore: moving env logic for migrations

--- src/config/database/connection.js
+++ src/config/database/connection.js
@@ -1,11 +1,7 @@
 import postgres from "pg";
 
 const connection = new postgres.Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "jit-model",
-  password: "jit",
-  port: "5432",
+  connectionString: process.env.DATABASE_URL
 });
 
 try {
