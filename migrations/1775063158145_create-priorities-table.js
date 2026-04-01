4d2520f0c18206029a0aab38fde97ad0656848fb
Emilson Filho <emilsonfilhocontato@gmail.com>
2026-04-01 14:36:19 -0300

    feat: implementing database migrations

---
+++ migrations/1775063158145_create-priorities-table.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.sql(`
+        CREATE TABLE priorities (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) NOT NULL UNIQUE
+        )
+    `)
+
+    pgm.sql(` INSERT INTO priorities (name) VALUES ('Baixa'), ('Média'), ('Alta')`)
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.sql(`DROP TABLE priorities`)
+};
